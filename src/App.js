89fa467f31581a4ce9adc8c9d1a70f1f5ed8e6e7
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-03-14 21:16:00 -0500

    Title, links

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>VANNA OH!'S NEAT WEBSITE</h1>
         <img src={lindsay} className="App-logo" alt="logo" />
         <p>
-          UNDER CONSTRUCTION
+          We are working on making this website real goddamn cool. 
         </p>
+        <ul>
+          <li><a href="https://vannaoh.bandcamp.com/merch">MERCH</a></li>
+          <li><a href="https://www.youtube.com/watch?v=Hphz0jDpxGU">WATCH</a></li>
+          <li><a href="https://open.spotify.com/artist/6hAuQeCv7JIhulgsOHWd3I?si=zYsuG-QSQbSxtjt7fdhDwg">LISTEN</a></li>
+        </ul>
        
       </header>
     </div>
